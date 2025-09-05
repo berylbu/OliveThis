91ce2e152c06b7d934aa14ee5c2da89ed7889200
Beryl <berylabucher@gmail.com>
2025-09-05 15:48:41 -0700

    added start rating view and emoji view

---
+++ OliveThis/Views/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  OliveThis
+//
+//  Created by Beryl Bucher on 9/5/25.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
